e52e07888fc46a14ea82f163e14317bda108b01f
Nikhilpogaku <nikhilpogaku24@gmail.com>
2022-01-01 09:57:25 +0000

    commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from random import randint
+from datetime import datetime, timedelta
+
+# Define the start and end dates
+start_date = datetime(year=2022, month=1, day=1)
+end_date = datetime(year=2023, month=1, day=24)
+
+# Calculate the number of days in the range
+delta_days = (end_date - start_date).days
+
+# Loop through each day from start_date to end_date
+for i in range(delta_days + 1):
+    current_date = start_date + timedelta(days=i)
+    d = current_date.strftime('%Y-%m-%d') + ' days ago'
+    for j in range(randint(1, 10)):
+        with open('file.txt', 'a') as file:
+            file.write(d + '\n')  # Added a newline character for better readability
+            os.system('git add .')
+            os.system('git commit --date="' + current_date.strftime('%Y-%m-%d') + '" -m "commit"')
+
+os.system('git push -u origin main')
